8e706addba6f04047ca8bd863e4f3cbaabaf4cf2
kienND9 <KienND9@fsoft.com.vn>
2017-03-01 15:51:26 +0700

    update source

---
+++ FakeYouTuBe/FakeYouTuBe/Cell/VideoCell.swift
@@ -0,0 +1,25 @@
+//
+//  VideoCell.swift
+//  FakeYouTuBe
+//
+//  Created by kienND9 on 3/1/17.
+//  Copyright © 2017 kienND9. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UICollectionViewCell{
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        
+    }
+}
--- FakeYouTuBe/FakeYouTuBe/ViewController.swift
+++ FakeYouTuBe/FakeYouTuBe/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Home"
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: "cellId")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
     }
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
 }
 
