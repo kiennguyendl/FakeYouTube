03cbefc80637d9b6ed3d866e7bdcfb85cc206f43
kienND9 <KienND9@fsoft.com.vn>
2017-03-03 17:17:36 +0700

    update source

--- FakeYouTuBe/FakeYouTuBe/Cell/VideoCell.swift
+++ FakeYouTuBe/FakeYouTuBe/Cell/VideoCell.swift
@@ -19,7 +19,42 @@ class VideoCell: UICollectionViewCell{
         fatalError("init(coder:) has not been implemented")
     }
     
+    let thumnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.blue
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     func setupView() {
+        addSubview(thumnailImageView)
+        addSubview(separatorView)
+        //thumnailImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[imageView]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["imageView": thumnailImageView]))
         
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-16-[imageView]-16-[view(1)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["imageView": thumnailImageView,"view": separatorView]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[imageView]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["imageView": separatorView]))
+        
+        //addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[imageView(1)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["imageView": separatorView]))
+    }
+}
+
+extension UIView{
+    func addContraintsWithFormat(format: String, views: UIView...)  {
+        var viewsDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated(){
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
     }
 }
--- FakeYouTuBe/FakeYouTuBe/ViewController.swift
+++ FakeYouTuBe/FakeYouTuBe/ViewController.swift
@@ -25,12 +25,16 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        cell.backgroundColor = UIColor.red
+        //cell.backgroundColor = UIColor.red
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
 
